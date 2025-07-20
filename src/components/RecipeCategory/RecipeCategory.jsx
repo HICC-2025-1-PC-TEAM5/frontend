7f8bef58bd4314dd09ad7f217dd9fe0bcb57b506
Yoon Ji-Yoon <dbswldbs0723@gmail.com>
2025-07-20 15:07:21 +0900

    레시피브랜치생성

---
+++ src/components/RecipeCategory/RecipeCategory.jsx
@@ -0,0 +1,24 @@
+import { ReactComponent as HansikBlack } from "../../assets/svg/hansik_black.svg";
+import { ReactComponent as HansikGray } from "../../assets/svg/hansik_gray.svg";
+import { ReactComponent as JungsikBlack } from "../../assets/svg/jungsik_black.svg";
+import { ReactComponent as JungsikGray } from "../../assets/svg/jungsik_gray.svg";
+import { ReactComponent as IlsikBlack } from "../../assets/svg/ilsik_black.svg";
+import { ReactComponent as IlsikGray } from "../../assets/svg/ilsik_gray.svg";
+
+export const categories = [
+  {
+    id: "한식",
+    blackIcon: HansikBlack,
+    grayIcon: HansikGray,
+  },
+  {
+    id: "중식",
+    blackIcon: JungsikBlack,
+    grayIcon: JungsikGray,
+  },
+  {
+    id: "일식",
+    blackIcon: IlsikBlack,
+    grayIcon: IlsikGray,
+  },
+];
