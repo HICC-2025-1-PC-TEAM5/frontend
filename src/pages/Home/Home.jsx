a645393bbcb717cb45c17c194494fe350e1547f4
Yoon Ji-Yoon <dbswldbs0723@gmail.com>
2025-08-01 15:41:09 +0900

    홈 화면 완성

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,8 +4,23 @@ import styles from './Home.module.css';
 import Nav from '../../components/Nav';
 import LogoIcon from '../../assets/svg/Main/logo.svg?react';
 import SearchInput from './components/SearchInput';
+import AlertCard from './components/AlertCard';
+import ImageCoin from '../../components/ImageCoin';
+import ImageCard from '../../components/ImageCard';
 
 export default function Main({ username = '홍길동' }) {
+  const navigate = useNavigate();
+  const imageData = [
+    { id: 1, imageSrc: '', text: '요리1', variant: 'large' },
+    { id: 2, imageSrc: '', text: '요리2', variant: 'large' },
+    { id: 3, imageSrc: '', text: '요리3', variant: 'large' },
+    { id: 4, imageSrc: '', text: '요리4', variant: 'large' },
+  ];
+
+  const handleCardClick = (id) => {
+    navigate(`/recipes/${id}`);
+  };
+
   return (
     <>
       <div className={styles.header}>
@@ -18,7 +33,30 @@ export default function Main({ username = '홍길동' }) {
           <br />
           어떤 음식을 요리해볼까요?
         </h1>
+
         <SearchInput onSearch={(value) => console.log(value)} />
+        <AlertCard onClick={() => navigate('/fridge')} />
+
+        <h3 className={styles.recommendTitle}>레시피 추천</h3>
+        <div className={styles.recipeList}>
+          <ImageCoin imageSrc="" text="15분 이내" variant="medium" />
+          <ImageCoin imageSrc="" text="든든 한끼" variant="medium" />
+          <ImageCoin imageSrc="" text="추천3" variant="medium" />
+          <ImageCoin imageSrc="" text="추천4" variant="medium" />
+          <ImageCoin imageSrc="" text="추천5" variant="medium" />
+        </div>
+        <h3 className={styles.recommendTitle}>요즘 많이 찾는 레시피</h3>
+        <div className={styles.content}>
+          {imageData.map((item) => (
+            <div key={item.id} onClick={() => handleCardClick(item.id)}>
+              <ImageCard
+                imageSrc={item.imageSrc}
+                text={item.text}
+                variant={item.variant}
+              />
+            </div>
+          ))}
+        </div>
       </div>
       <Nav />
     </>
