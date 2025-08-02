9c6cb53543618e0ff99bc714e2642d9152b34e36
Wany <sung@wanyne.com>
2025-08-02 20:04:39 +0900

    냉장고 페이지 작업

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -2,21 +2,26 @@ import styles from './Stack.module.css';
 
 export default (props) => {
   const {
+    className,
     direction = 'horizontal',
     justify,
     align,
     gap = 'normal',
     wrap,
+    rows,
+    cols,
   } = props;
 
   return (
     <div
-      className={styles.stack}
+      className={`${styles.stack} ${className}`}
       direction={direction}
       justify={justify}
       align={align}
       gap={gap}
       wrap={wrap}
+      rows={rows}
+      cols={cols}
     >
       {props.children}
     </div>
